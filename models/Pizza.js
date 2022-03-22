78415a4bb6f4694faf9e5db799dc110c100df43e
Chidori <business.faissal@outlook.com>
2022-03-21 23:08:13 -0500

    created comment model

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,21 +1,47 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Acts as middleware to transform the data before it gets to the controllers.
+      // Every time a pizza is retrieved, the value will be formatted by the dateFormat()
+      // and used instead of the default timestamp.
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    size: {
+      type: String,
+      default: 'Large',
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        // tells the Pizza model which documents to search to find the right comments
+        ref: 'Comment',
+      },
+    ],
   },
-  createdBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: 'Large',
-  },
-  toppings: [],
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
 });
 
 // creates the Pizza model using the PizzaSchema from above
